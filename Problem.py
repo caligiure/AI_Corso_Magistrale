b682d781b29ce6a7a3a87224866c69cba0d79da0
caligiure <caligiure74@gmail.com>
2026-03-16 13:41:08 +0100

    Defining Problem abstract class

---
+++ Problem.py
@@ -0,0 +1,56 @@
+class Problem(object):
+    """The abstract class for a formal problem. A new domain subclasses this,
+    overriding `actions` and `results`, and perhaps other methods.
+    The default heuristic is 0 and the default action cost is 1 for all states.
+    When yiou create an instance of a subclass, specify `initial`, and `goal` states
+    (or give an `is_goal` method) and perhaps other keyword args for the subclass."""
+
+    def __init__(self, initial=None, goal=None, **kwds):
+        self.__dict__.update(initial=initial, goal=goal, **kwds)
+
+    def actions(self, state):        raise NotImplementedError # returns a list of actions
+    def result(self, state, action): raise NotImplementedError # returns the state that results from taking an action in a state
+    def is_goal(self, state):        return state == self.goal # returns True if the state is the goal state
+    def action_cost(self, s, a, s1): return 1 # returns the cost of an action
+    def h(self, node):               return 0 # returns the heuristic of a node
+
+    def __str__(self):
+        return '{}({!r}, {!r})'.format(
+            type(self).__name__, self.initial, self.goal) # name of the class and the initial and goal states
+
+class Node:
+    "A Node in a search tree."
+    def __init__(self, state, parent=None, action=None, path_cost=0): # path_cost is the total cumulated cost from start to this node
+        self.__dict__.update(state=state, parent=parent, action=action, path_cost=path_cost)
+
+    def __repr__(self): return '<{}>'.format(self.state) # state representation
+    def __len__(self): return 0 if self.parent is None else (1 + len(self.parent)) # length of the path
+    def __lt__(self, other): return self.path_cost < other.path_cost # LT = less than (used for priority queue)
+
+
+# sentinel nodes used to signal special conditions and avoid crashes (the infinity ensures that these nodes are never chosen as the best node in a path)
+failure = Node('failure', path_cost=math.inf) # Indicates an algorithm couldn't find a solution.
+cutoff  = Node('cutoff',  path_cost=math.inf) # Indicates iterative deepening search was cut off.
+
+
+def expand(problem, node):
+    "Expand a node, generating the children nodes."
+    s = node.state
+    for action in problem.actions(s):
+        s1 = problem.result(s, action)
+        cost = node.path_cost + problem.action_cost(s, action, s1)
+        yield Node(s1, node, action, cost) # yield (instead of return) is used to generate the children nodes one by one
+
+
+def path_actions(node):
+    "The sequence of actions to get to this node."
+    if node.parent is None:
+        return []
+    return path_actions(node.parent) + [node.action]
+
+
+def path_states(node):
+    "The sequence of states to get to this node."
+    if node in (cutoff, failure, None):
+        return []
+    return path_states(node.parent) + [node.state]
